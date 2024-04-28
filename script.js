8b6c9decbf15d31a69c20a7f5975d97171919ddc
Vijay Mehra <vijaymehralm2004@gmail.com>
2024-04-28 22:38:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+// animating the section headings
+
+const headings = document.querySelectorAll("h2");
+
+headings.forEach((heading)=>{
+  heading.classList.add("animated-heading");
+})
+
+
